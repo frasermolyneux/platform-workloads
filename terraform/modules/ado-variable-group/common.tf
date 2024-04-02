7464c3ca7663589517247835aae7433a2335ba85
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-04-02 21:08:11 +0100

    Update byte length for environment_id resource

--- terraform/modules/ado-variable-group/common.tf
+++ terraform/modules/ado-variable-group/common.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {}
 data "azuread_client_config" "current" {}
 
 resource "random_id" "environment_id" {
-  byte_length = 6
+  byte_length = 5
 }
 
 resource "time_rotating" "rotate" {
