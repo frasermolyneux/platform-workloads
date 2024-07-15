866c0cb9bc1a00832e22afc9a518e3f145cc191f
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-15 20:05:47 +0100

    Add new resources

--- terraform/azure-workloads.if-devops.tf
+++ terraform/azure-workloads.if-devops.tf
@@ -44,11 +44,50 @@ resource "azuredevops_serviceendpoint_azurerm" "workload" {
   azurerm_subscription_name = var.subscriptions[each.value.subscription].name
 }
 
-resource "azuredevops_pipeline_authorization" "workload" {
+moved {
+  from = azuredevops_pipeline_authorization.workload
+  to   = azuredevops_pipeline_authorization.workload_auth_to_serviceendpoint
+}
+
+resource "azuredevops_pipeline_authorization" "workload_auth_to_serviceendpoint" {
   for_each = { for each in local.workload_environments : each.key => each if each.connect_to_devops }
 
   project_id  = azuredevops_project.project[each.value.devops_project].id
   resource_id = azuredevops_serviceendpoint_azurerm.workload[each.key].id
 
   type = "endpoint"
+}
+
+resource "azuredevops_variable_group" "workload" {
+  for_each = { for each in local.workload_environments : each.key => each if each.connect_to_devops }
+
+  project_id  = azuredevops_project.project[each.value.devops_project].id
+  name        = azuread_application.workload[each.key].display_name
+  description = "Managed by frasermolyneux/platform-workloads"
+
+  allow_access = false
+
+  variable {
+    name  = "AZURE_CLIENT_ID"
+    value = azuread_application.workload[each.key].application_id
+  }
+
+  variable {
+    name  = "AZURE_SUBSCRIPTION_ID"
+    value = var.subscriptions[each.value.subscription].subscription_id
+  }
+
+  variable {
+    name  = "AZURE_TENANT_ID"
+    value = "e56a6947-bb9a-4a6e-846a-1f118d1c3a14"
+  }
+}
+
+resource "azuredevops_pipeline_authorization" "workload_auth_to_variable_group" {
+  for_each = { for each in local.workload_environments : each.key => each if each.connect_to_devops }
+
+  project_id  = azuredevops_project.project[each.value.devops_project].id
+  resource_id = azuredevops_variable_group.workload[each.value.devops_project].id
+
+  type = "variablegroup"
 }
