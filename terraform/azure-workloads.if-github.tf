94a9ab1e4cb122dda237838d27b84cefeba65da5
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-12-22 10:21:28 +0000

    Move from secrets to environment variables

--- terraform/azure-workloads.if-github.tf
+++ terraform/azure-workloads.if-github.tf
@@ -21,54 +21,29 @@ resource "github_repository_environment" "workload" {
   repository  = github_repository.workload[each.value.workload_name].name
 }
 
-resource "github_actions_environment_secret" "client_id" {
+resource "github_actions_environment_variable" "client_id" {
   for_each = { for each in local.workload_environments : each.key => each if each.connect_to_github }
 
-  repository      = github_repository.workload[each.value.workload_name].name
-  environment     = github_repository_environment.workload[each.key].environment
-  secret_name     = "AZURE_CLIENT_ID"
-  plaintext_value = azuread_application.workload[each.key].client_id
+  repository    = github_repository.workload[each.value.workload_name].name
+  environment   = github_repository_environment.workload[each.key].environment
+  variable_name = "AZURE_CLIENT_ID"
+  value         = azuread_application.workload[each.key].client_id
 }
 
-resource "github_actions_environment_secret" "subscription_id" {
+resource "github_actions_environment_variable" "subscription_id" {
   for_each = { for each in local.workload_environments : each.key => each if each.connect_to_github }
 
-  repository      = github_repository.workload[each.value.workload_name].name
-  environment     = github_repository_environment.workload[each.key].environment
-  secret_name     = "AZURE_SUBSCRIPTION_ID"
-  plaintext_value = var.subscriptions[each.value.subscription].subscription_id
+  repository    = github_repository.workload[each.value.workload_name].name
+  environment   = github_repository_environment.workload[each.key].environment
+  variable_name = "AZURE_SUBSCRIPTION_ID"
+  value         = var.subscriptions[each.value.subscription].subscription_id
 }
 
-resource "github_actions_environment_secret" "tenant_id" {
+resource "github_actions_environment_variable" "tenant_id" {
   for_each = { for each in local.workload_environments : each.key => each if each.connect_to_github }
 
-  repository      = github_repository.workload[each.value.workload_name].name
-  environment     = github_repository_environment.workload[each.key].environment
-  secret_name     = "AZURE_TENANT_ID"
-  plaintext_value = "e56a6947-bb9a-4a6e-846a-1f118d1c3a14"
-}
-
-// if this is a development environment, also add the secrets as a dependabot secret
-resource "github_dependabot_secret" "client_id" {
-  for_each = { for each in local.workload_environments : each.key => each if each.connect_to_github && each.environment_name == "Development" }
-
-  repository      = github_repository.workload[each.value.workload_name].name
-  secret_name     = "AZURE_CLIENT_ID"
-  plaintext_value = azuread_application.workload[each.key].client_id
-}
-
-resource "github_dependabot_secret" "subscription_id" {
-  for_each = { for each in local.workload_environments : each.key => each if each.connect_to_github && each.environment_name == "Development" }
-
-  repository      = github_repository.workload[each.value.workload_name].name
-  secret_name     = "AZURE_SUBSCRIPTION_ID"
-  plaintext_value = var.subscriptions[each.value.subscription].subscription_id
-}
-
-resource "github_dependabot_secret" "tenant_id" {
-  for_each = { for each in local.workload_environments : each.key => each if each.connect_to_github && each.environment_name == "Development" }
-
-  repository      = github_repository.workload[each.value.workload_name].name
-  secret_name     = "AZURE_TENANT_ID"
-  plaintext_value = "e56a6947-bb9a-4a6e-846a-1f118d1c3a14"
+  repository    = github_repository.workload[each.value.workload_name].name
+  environment   = github_repository_environment.workload[each.key].environment
+  variable_name = "AZURE_TENANT_ID"
+  value         = "e56a6947-bb9a-4a6e-846a-1f118d1c3a14"
 }
