630f841a60b1ca225c3eca431a1f0483f803f8a4
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-04-01 20:33:55 +0100

    Refactor dev_center.tf to implement azapi resources for environment types and workloads

--- terraform/dev_center.tf
+++ terraform/dev_center.tf
@@ -1,18 +1,10 @@
-//locals {
-//  environment_types = [
-//    "Development",
-//    "Production",
-//  ]
-//}
-//
-//resource "azurerm_dev_center" "dev_center" {
-//  name = local.dev_center_name
-//
-//  location            = azurerm_resource_group.rg.location
-//  resource_group_name = azurerm_resource_group.rg.name
-//
-//  tags = var.tags
-//}
+locals {
+  environment_types = [
+    "Development",
+    "Production",
+  ]
+}
+
 resource "azapi_resource" "dev_center" {
   type      = "Microsoft.DevCenter/devcenters@2025-02-01"
   parent_id = azurerm_resource_group.rg.id
@@ -28,43 +20,64 @@ resource "azapi_resource" "dev_center" {
     }
   }
 }
-//
-//resource "azurerm_dev_center_environment_type" "environment" {
-//  for_each = { for each in local.environment_types : each => each }
-//
-//  name          = each.value
-//  dev_center_id = azurerm_dev_center.dev_center.id
-//
-//  tags = {
-//    Environment = each.value
-//  }
-//}
-//
-//resource "azurerm_dev_center_project" "workload" {
-//  for_each = { for each in var.workloads : each.name => each if each.create_dev_center_project }
-//
-//  name = each.value.name
-//
-//  dev_center_id = azurerm_dev_center.dev_center.id
-//
-//  location            = azurerm_resource_group.rg.location
-//  resource_group_name = azurerm_resource_group.rg.name
-//
-//  tags = merge(var.tags, { Workload = each.value.name })
-//}
-//
-//resource "azurerm_dev_center_project_environment_type" "project_environment" {
-//  for_each = { for each in local.workload_environments : each.key => each if each.create_dev_center_project }
-//
-//  name     = azurerm_dev_center_environment_type.environment[each.value.environment_name].name
-//  location = azurerm_resource_group.rg.location
-//
-//  dev_center_project_id = azurerm_dev_center_project.workload[each.value.workload_name].id
-//  deployment_target_id  = data.azurerm_subscription.subscriptions[each.value.subscription].id
-//
-//  identity {
-//    type = "SystemAssigned"
-//  }
-//
-//  tags = merge(azurerm_dev_center_environment_type.environment[each.value.environment_name].tags, { Workload = each.value.workload_name })
-//}
+
+resource "azapi_resource" "environment_type" {
+  for_each = { for each in local.environment_types : each => each }
+
+  type      = "Microsoft.DevCenter/devcenters/environmentTypes@2025-02-01"
+  parent_id = azapi_resource.dev_center.id
+
+  name     = each.value
+  location = azurerm_resource_group.rg.location
+
+  tags = {
+    Environment = each.value
+  }
+
+  body = {
+    properties = {}
+  }
+}
+
+resource "azapi_resource" "workload" {
+  for_each = { for each in var.workloads : each.name => each if each.create_dev_center_project }
+
+  type      = "Microsoft.DevCenter/devcenters/projects@2025-02-01"
+  parent_id = azapi_resource.dev_center.id
+
+  name     = each.value.name
+  location = azurerm_resource_group.rg.location
+
+  tags = merge(var.tags, { Workload = each.value.name })
+
+  body = {
+    properties = {
+      description = "Project for workload ${each.value.name}"
+    }
+  }
+}
+
+resource "azapi_resource" "project_environment" {
+  for_each = { for each in local.workload_environments : each.key => each if each.create_dev_center_project }
+
+  type      = "Microsoft.DevCenter/devcenters/projects/environmentTypes@2025-02-01"
+  parent_id = azapi_resource.workload[each.value.workload_name].id
+
+  name     = azapi_resource.environment_type[each.value.environment_name].name
+  location = azurerm_resource_group.rg.location
+
+  tags = merge(
+    azapi_resource.environment_type[each.value.environment_name].tags,
+    { Workload = each.value.workload_name }
+  )
+
+  body = {
+    properties = {
+      deploymentTargetId = data.azurerm_subscription.subscriptions[each.value.subscription].id
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
