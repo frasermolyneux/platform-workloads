8bd2d28a11cf7f3d5a3534984c5391061196e936
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-10-05 15:00:55 +0100

    Add RBAC administrator configurations for workloads and update role assignments

---
+++ terraform/azure-workloads.rbac.tf
@@ -0,0 +1,83 @@
+locals {
+  workload_rbac_administrator_map = {
+    for environment in local.workload_environments :
+    environment.key => [
+      for entry in try(environment.rbac_administrator, []) : {
+        workload_name          = environment.workload_name
+        environment_name       = environment.environment_name
+        service_principal_name = format("spn-%s-%s", lower(environment.workload_name), lower(environment.environment_name))
+        scope_name             = entry.scope
+        scope_id               = try(data.azurerm_subscription.subscriptions[entry.scope].id, entry.scope)
+        subscription_id        = try(data.azurerm_subscription.subscriptions[entry.scope].subscription_id, null)
+        allowed_roles          = try(entry.allowed_roles, [])
+      }
+    ]
+    if length(try(environment.rbac_administrator, [])) > 0
+  }
+
+  workload_rbac_allowed_role_map = {
+    for request in flatten([
+      for environment in local.workload_environments : [
+        for entry in try(environment.rbac_administrator, []) : [
+          for role_name in try(entry.allowed_roles, []) : {
+            key        = format("%s|%s", entry.scope, role_name)
+            scope_name = entry.scope
+            role_name  = role_name
+          }
+        ]
+      ]
+    ]) :
+    request.key => {
+      scope_name = request.scope_name
+      role_name  = request.role_name
+    }
+  }
+
+  workload_rbac_administrator_assignments = flatten([
+    for environment in local.workload_environments : [
+      for entry_index, entry in try(environment.rbac_administrator, []) : {
+        assignment_key           = format("%s-%s-%d", environment.key, replace(entry.scope, "/", "-"), entry_index)
+        workload_environment_key = environment.key
+        scope_id                 = try(data.azurerm_subscription.subscriptions[entry.scope].id, entry.scope)
+        allowed_role_keys = [
+          for role_name in try(entry.allowed_roles, []) :
+          format("%s|%s", entry.scope, role_name)
+        ]
+      }
+      if length(try(entry.allowed_roles, [])) > 0
+    ]
+  ])
+}
+
+data "azurerm_role_definition" "workload_rbac_allowed" {
+  for_each = local.workload_rbac_allowed_role_map
+
+  name  = each.value.role_name
+  scope = try(data.azurerm_subscription.subscriptions[each.value.scope_name].id, each.value.scope_name)
+}
+
+resource "azurerm_role_assignment" "workload_rbac_administrator" {
+  for_each = { for assignment in local.workload_rbac_administrator_assignments : assignment.assignment_key => assignment }
+
+  scope                = each.value.scope_id
+  role_definition_name = "Role Based Access Control Administrator"
+  principal_id         = azuread_service_principal.workload[each.value.workload_environment_key].object_id
+
+  condition_version = "2.0"
+  condition = jsonencode({
+    AllOf = [
+      {
+        AnyOf = [
+          for role_key in each.value.allowed_role_keys : {
+            Field  = "Microsoft.Authorization/roleAssignments/roleDefinitionId"
+            Equals = data.azurerm_role_definition.workload_rbac_allowed[role_key].id
+          }
+        ]
+      }
+    ]
+  })
+}
+
+output "workload_rbac_administrators" {
+  value = local.workload_rbac_administrator_map
+}
