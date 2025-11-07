55340e9ea8a34aa4bcbbd438affc6388b0635356
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-11-07 20:28:08 +0000

    Refactor role assignments and resource groups in molyneux-me configuration

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -2,6 +2,8 @@ import {
   for_each = var.environment == "prd" ? {
     "portal-environments-Development-uksouth-rg-portal-environments-dev-uksouth" = "/subscriptions/d68448b0-9947-46d7-8771-baa331a3063a/resourceGroups/rg-portal-environments-dev-uksouth"
     "portal-environments-Production-uksouth-rg-portal-environments-prd-uksouth"  = "/subscriptions/32444f38-32f4-409f-889c-8e8aa2b5b4d1/resourceGroups/rg-portal-environments-prd-uksouth"
+    "portal-environments-Development-uksouth-rg-portal-environments-dev-uksouth" = "/subscriptions/d68448b0-9947-46d7-8771-baa331a3063a/resourceGroups/rg-portal-environments-dev-uksouth"
+    "portal-environments-Production-uksouth-rg-portal-environments-prd-uksouth"  = "/subscriptions/32444f38-32f4-409f-889c-8e8aa2b5b4d1/resourceGroups/rg-portal-environments-prd-uksouth"
   } : {}
 
   to = azapi_resource.workload_resource_group[each.key]
