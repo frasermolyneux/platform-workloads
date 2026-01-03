a94d8fb675322c790df48554aff3bc2e2a7e15c3
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-03 19:15:29 +0000

    Remove unused Terraform import configurations for platform monitoring and portal environments

--- terraform/imports.platform-monitoring.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = azapi_resource.workload_resource_group["platform-monitoring-Development-uksouth-rg-platform-monitoring-dev-uksouth"]
-  id = format("%s/resourceGroups/%s", data.azurerm_subscription.subscriptions["sub-visualstudio-enterprise"].id, "rg-platform-monitoring-dev-uksouth")
-}
-
-import {
-  to = azapi_resource.workload_resource_group["platform-monitoring-Production-uksouth-rg-platform-monitoring-prd-uksouth"]
-  id = format("%s/resourceGroups/%s", data.azurerm_subscription.subscriptions["sub-platform-management"].id, "rg-platform-monitoring-prd-uksouth")
-}
